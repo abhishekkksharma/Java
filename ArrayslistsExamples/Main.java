1db9a69975552232de8e2b208eb42a44d46d3dfd
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-09-04 15:45:52 +0530

    add arraylists

---
+++ ArrayslistsExamples/Main.java
@@ -0,0 +1,30 @@
+package ArrayslistsExamples;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+        Scanner in = new Scanner(System.in);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println(list.contains(10));
+        System.out.println(list);
+        list.set(0, 99);
+        // for (Integer integer : list) {
+        //     System.out.print(integer+" ");
+        // }
+        System.out.println(list);
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " "); // pass index here, list[index] wont work here 
+        }
+        // System.out.println(list);
+    }
+}
