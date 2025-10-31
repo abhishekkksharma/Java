e3be495048e17223036f05817f2b8c697125f4fb
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-10-31 10:09:59 +0530

    add stacks and queues

---
+++ StacksAndQueues/InBuiltStacks/Stacks.java
@@ -0,0 +1,20 @@
+package StacksAndQueues.InBuiltStacks;
+
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack.pop());  //LIFO 
+        System.out.println(stack.pop()); 
+        System.out.println(stack.pop()); 
+        System.out.println(stack.pop()); 
+        System.out.println(stack.pop());
+    }
+}
---
+++ StacksAndQueues/Queues/Queues.java
@@ -0,0 +1,13 @@
+package StacksAndQueues.Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(5);
+        System.out.println(queue.remove());
+    }
+}
